f5ed90bbac4694e09ed505610574c36b38d578f1
rxnaij <richardblue37@gmail.com>
2020-12-02 18:06:53 -0500

    add some styling to editor

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -3,9 +3,13 @@
  */
 
 import * as React from 'react'
-import { Editor, EditorState, RichUtils } from 'draft-js'
+import { Editor, EditorState, RichUtils, ContentBlock } from 'draft-js'
 import "draft-js/dist/Draft.css";
 import "./Editor.scss"
+import classNames from 'classnames'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBold, faItalic, faHeading } from '@fortawesome/free-solid-svg-icons'
 
 /**
  * Component props handle the app state.
@@ -19,24 +23,27 @@ interface EditorProps {
 
 interface EditorOption {
     name: string,
-    label: "",
-    fn: (e: React.MouseEvent) => void
+    icon: JSX.Element,
+    fn: (e: React.MouseEvent) => void,
+    isActive: boolean|null
 }
 
 export default function MyEditor({ value, onChange, setText, readOnly }: EditorProps) {
     // State of editor contents
     const editor = React.useRef<Editor>(null)
 
+    // Sends plaintext value of content to parent component
     React.useEffect(() => {
         const words = value.getCurrentContent().getPlainText()
-        console.log(typeof words)
         setText && setText(words)
     }, [value])
 
+    /** @todo  Focus fix for editor (WIP) */
     function focusEditor() {
         if (editor && editor.current !== null) editor.current.focus();
     }
 
+    // Handles key commands entered in the editor (like ctrl+B for bolding)
     const handleKeyCommand = (command: string, value: EditorState) => {
         const newState = RichUtils.handleKeyCommand(value, command)
         if (newState) {
@@ -46,25 +53,43 @@ export default function MyEditor({ value, onChange, setText, readOnly }: EditorP
         return 'not-handled'
     }
 
-    // Text rendering options to render
-    /** @todo: decide which options to add */
+    // Text styling options
     const options: EditorOption[] = [
         {
             name: "Bold",
-            label: "",
+            icon: <FontAwesomeIcon icon={faBold} />,
             fn: () => onChange(RichUtils.toggleInlineStyle(value, 'BOLD')),
+            isActive: false
         },
         {
             name: "Italic",
-            label: "",
-            fn: () => onChange(RichUtils.toggleInlineStyle(value, 'ITALIC'))
+            icon: <FontAwesomeIcon icon={faItalic} />,
+            fn: () => onChange(RichUtils.toggleInlineStyle(value, 'ITALIC')),
+            isActive: false
         },
         {
             name: "H1",
-            label: "",
-            fn: () => onChange(RichUtils.toggleBlockType(value, 'header-one'))
+            icon: <FontAwesomeIcon icon={faHeading} />,
+            fn: () => onChange(RichUtils.toggleBlockType(value, 'header-one')),
+            isActive: false
         },
     ]
+
+    /** 
+     * Handles styling of block-level elements in the text.
+     * Returns a CSS class corresponding to the selected type.
+     */ 
+    const blockStyler = (contentBlock: ContentBlock): string => {
+        const type = contentBlock.getType()
+        switch (type) {
+            case 'paragraph':
+                return 'editor-text--p'
+            case 'header-one':
+                return 'editor-text--header-one'
+            default:
+                return 'editor-text--p'
+        }
+    }
 
     return(
         <div
@@ -76,17 +101,28 @@ export default function MyEditor({ value, onChange, setText, readOnly }: EditorP
                  * @todo: return focus to editor after button is clicked. Currently, onClick handlers
                  * steal focus from the editor, so they don't work. onMouseDown is kind of hacky.
                  */
-                !readOnly && options.map(option => (
-                    <button key={`editor-option--${option.name}`} onMouseDown={option.fn}>
-                        {option.name}
-                    </button>
-                ))
+                !readOnly && 
+                <fieldset className="editor__options-bar">{
+                    options.map(option => (
+                        <button
+                            key={`editor-option--${option.name}`} 
+                            onMouseDown={option.fn} 
+                            className={classNames(
+                                "editor__option", 
+                                value.getCurrentInlineStyle().find(k => k === option.name.toUpperCase()) && "editor__option--is-active" // For inline styles: highlights button if that style is present in the current selection
+                            )}
+                        >
+                            {option.icon}
+                        </button>
+                    ))
+                }</fieldset>
             }
             <Editor
                 ref={editor}
                 editorState={value}
                 onChange={onChange}
                 handleKeyCommand={handleKeyCommand}
+                blockStyleFn={blockStyler}
                 placeholder="Write something!"
                 readOnly={readOnly}
             />
